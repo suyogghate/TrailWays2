666130ebbf90d3f34e4e6de88b2fdeacd9f10eef
Ghate Suyog <suyogghateminks@gmail.com>
2023-10-27 00:58:44 +0530

    Initial table trek

---
+++ Trek.sql
@@ -0,0 +1,11 @@
+create database trailways;
+USE trailways;
+CREATE table Trek(
+TrekkerID int primary key,
+Name varchar(20),
+Email varchar(20),
+Mobile varchar(10),
+AadharCard varchar(12),
+Password varchar(20)
+);
+
